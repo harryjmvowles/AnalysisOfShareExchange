9dc694eae859e3f813cf157f06f29535a8dff8aa
harryjmvowles <29339644@students.lincoln.ac.uk>
2025-03-25 14:05:03 +0000

    Added Share Files to debug folder, Created FileReader class, Created array inisitaliser for 256 and 2048.

---
+++ FileReader.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalysisOfShareExchange
+{
+    class FileReader
+    {
+        private string[] fileNames;
+
+        //constructor to initialise the fileNames.
+        public FileReader(string[] fileNames)
+        {
+            this.fileNames = fileNames;
+        }
+
+        //Method to read and filter numbers from files based on type(256 or 2048).
+        public int[] ArrayFromFile(string fileTypes)
+        {
+            //Inistialise a list to store the numbers.
+            List<int> numbersList = new List<int>();
+            
+            foreach (string file in fileNames)
+            {
+                //Checks if the file exists and if it contains the correct type (256 or 2048).
+                if (file.Contains(fileTypes) && File.Exists(file))
+                {
+                    string[] lines = File.ReadAllLines(file);
+                    foreach (string line in lines)
+                    {
+                        if (int.TryParse(line, out int number))
+                        {
+                            numbersList.Add(number); //Add the number to the list.
+                        }
+                    }
+                }
+                else if (!file.Contains(fileTypes))
+                {
+                    continue; //Skip files that don't match the type
+                }
+                else
+                {
+                    Console.WriteLine($"File not found: {file}");
+                }
+            }
+            return numbersList.ToArray();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,6 +12,17 @@ namespace AnalysisOfShareExchange
     {
         static void Main(string[] args)
         {
+            //File Names to Access both 256 and 2048 numbers.
+            string[] fileNames = { "Share_1_256.txt", "Share_2_256.txt", "Share_3_256.txt", "Share_1_2048.txt", "Share_2_2048.txt", "Share_3_2048.txt" };
+
+            //Creating an instance of the FileReader class and Calls ArrayFromFile method to create an
+            //array of numbers from the file.
+            FileReader fileReader = new FileReader(fileNames);
+            int[] numbersArray256 = fileReader.ArrayFromFile("256");
+            int[] numbersArray2048 = fileReader.ArrayFromFile("2048");
+
+            Console.WriteLine("Numbers from 256 files: " + string.Join(", ", numbersArray256));
+            Console.WriteLine("Numbers from 2048 files: " + string.Join(", ", numbersArray2048));
 
         }
     }
